e21dc478df629f466479a15dbe5a02fc381a2e8e
Renzo Martin Rosas Vidal <mfthabits4life@hotmail.com>
2020-06-04 16:49:27 -0500

    3er dia

--- lib/widgets/lista_noticias.dart
+++ lib/widgets/lista_noticias.dart
@@ -14,8 +14,53 @@ class ListaNoticias extends StatelessWidget {
       return ListView.builder(
         itemCount: this.noticias.length,  
         itemBuilder: (BuildContext context, int index){
-          return Text(this.noticias[index].title);
+
+          return _Noticia(noticia:this.noticias[index], index: index,);
+
         }
       );
+  }
+
+
+}
+
+class _Noticia extends StatelessWidget {
+
+  final Article noticia;
+  final int index;
+
+  const _Noticia({
+    @required this.noticia,
+    @required this.index
+    });
+  
+  @override
+  Widget build(BuildContext context) {
+
+      return Column(
+        children: <Widget>[
+          _TarjetaTopBar(noticia:noticia,)
+        ],
+      );
+
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopBar({this.noticia, this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Text('$index + 1')
+        ],
+      ),
+    );
   }
 }
